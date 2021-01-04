5e129b4cc0ecbe010fe2e6dd79b0c719978d2f88
Юрий Йосифович <aramic2007@gmail.com>
2021-01-04 23:04:37 +0200

    Update temp.js
    Формирование sitemap

---
+++ temp.js
@@ -0,0 +1,28 @@
+function sitemap(arrayLinks) {
+    let h2 = document.createElement('h2');
+    h2.innerHTML = 'Sitemap';
+    var table = document.createElement('table')
+    var thead = table.appendChild(document.createElement('thead'));
+    var tr = thead.appendChild(document.createElement('tr'));
+    var th = tr.appendChild(document.createElement('th'));
+        th.setAttribute('align', 'left');
+        th.setAttribute('style', 'width:50px');
+        th.innerHTML('#');
+        th = tr.appendChild(document.createElement('th'));
+        th.setAttribute('align', 'left');
+        th.innerHTML('URL');
+    var tbody = table.appendChild(document.createElement('tbody'));
+    if(arrayLinks['length'] > 0) {
+        for(i=0; i < arrayLinks['length']; i++) {
+            tr = tbody.appendChild(document.createElement('tr'));
+            var td = tr.appendChild(document.createElement('td'));
+            td.innerHTML = (i + 1);
+            td = tr.appendChild(document.createElement('td'));
+            var a = td.appendChild(document.createElement('a'));
+            a.setAttribute('rel', 'follow');
+            a.setAttribute('href', arrayLinks[i]);
+            a.innerHTML = arrayLinks[i];
+        }
+    }
+    main['innerHTML'] = h2 + table;
+}
