63b874712535dc59b608c313b410e9c1863d9989
Юрий Йосифович <aramic2007@gmail.com>
2021-01-06 02:49:47 +0200

    Update temp.js

--- temp.js
+++ temp.js
@@ -79,23 +79,23 @@ function GetData(dataUrl){
     // 4. Этот код сработает после того, как мы получим ответ сервера
     xhr.onload = function() {
       if (xhr.status != 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
-        alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); // Например, 404: Not Found
+        console.log(`Ошибка ${xhr.status}: ${xhr.statusText}`); // Например, 404: Not Found
       } else { // если всё прошло гладко, выводим результат
-        alert(`Готово, получили ${xhr.response.length} байт`); // response -- это ответ сервера
+        console.log(`Готово, получили ${xhr.response.length} байт`); // response -- это ответ сервера
       }
     };
 
     xhr.onprogress = function(event) {
       if (event.lengthComputable) {
-        alert(`Получено ${event.loaded} из ${event.total} байт`);
+        console.log(`Получено ${event.loaded} из ${event.total} байт`);
       } else {
-        alert(`Получено ${event.loaded} байт`); // если в ответе нет заголовка Content-Length
+        console.log(`Получено ${event.loaded} байт`); // если в ответе нет заголовка Content-Length
       }
 
     };
 
     xhr.onerror = function() {
-      alert("Запрос не удался");
+      console.log("Запрос не удался");
     };
     return xhr.response;
 }
