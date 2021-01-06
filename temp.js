e061bbb1b1e9cd660e656b2f57acab36720ca93e
Юрий Йосифович <aramic2007@gmail.com>
2021-01-06 02:40:11 +0200

    Update temp.js

--- temp.js
+++ temp.js
@@ -66,6 +66,39 @@ else {
 // https://project.nekaters.com/product/site.web.app/?callback=search
 // https://project.nekaters.com/assets/js/06-01-2021/firebase.js
 
+function GetData(dataUrl){
+    // 1. Создаём новый XMLHttpRequest-объект
+    let xhr = new XMLHttpRequest();
+    xhr.timeout = 10000;
+    // 2. Настраиваем его: GET-запрос по URL /article/.../load
+    xhr.open('GET', dataUrl);
+
+    // 3. Отсылаем запрос
+    xhr.send();
+
+    // 4. Этот код сработает после того, как мы получим ответ сервера
+    xhr.onload = function() {
+      if (xhr.status != 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
+        alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); // Например, 404: Not Found
+      } else { // если всё прошло гладко, выводим результат
+        alert(`Готово, получили ${xhr.response.length} байт`); // response -- это ответ сервера
+      }
+    };
+
+    xhr.onprogress = function(event) {
+      if (event.lengthComputable) {
+        alert(`Получено ${event.loaded} из ${event.total} байт`);
+      } else {
+        alert(`Получено ${event.loaded} байт`); // если в ответе нет заголовка Content-Length
+      }
+
+    };
+
+    xhr.onerror = function() {
+      alert("Запрос не удался");
+    };
+    return xhr.response;
+}
 
 
 // Подгрузка странички
